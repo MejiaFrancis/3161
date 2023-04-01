fdadd13dd55c1d243b8a0b3032392f5cf117a6a8
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-03-31 23:40:34 -0600

    all migrations tables have data being inserted

---
+++ quiz-2/recsystem/migrations/000018_insert_roles_data.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO roles (name)
+VALUES
+  ('Admin'),
+  ('Student'),
+  ('Faculty'),
+  ('Staff'),
+  ('Alumni'),
+  ('Guest'),
+  ('Researcher'),
+  ('Contractor'),
+  ('Intern'),
+  ('Volunteer');
