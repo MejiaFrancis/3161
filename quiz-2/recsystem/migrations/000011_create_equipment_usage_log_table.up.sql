b8fb59ca3ec1bd50dea8b70e5af49262694d2556
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-04-05 11:53:11 -0600

    Most migrations working- usage log need to be corrected

--- quiz-2/recsystem/migrations/000011_create_equipment_usage_log_table.up.sql
+++ quiz-2/recsystem/migrations/000011_create_equipment_usage_log_table.up.sql
@@ -1,7 +1,7 @@
 -- Filename: migrations/000013_create_equipment_usage_log_table.up.sql
 CREATE TABLE IF NOT EXISTS equipment_usage_log (
   equipment_usage_log_id bigserial PRIMARY KEY,
-  equipments_id bigint NOT NULL REFERENCES "equipment" ("equipments_id"),
+  equipments_id bigserial NOT NULL REFERENCES "equipments" ("equipments_id"),
   user_id bigserial NOT NULL REFERENCES "users" ("users_id"), 
   logs_id bigserial,
   time_borrowed TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
