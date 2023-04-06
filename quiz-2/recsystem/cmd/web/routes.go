e1c56e3d21fa12a57795df61757807d6f2adb8dc
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-04-05 18:17:57 -0600

    additional routes added but not written to indicate handlers that will need to be built

--- quiz-2/recsystem/cmd/web/routes.go
+++ quiz-2/recsystem/cmd/web/routes.go
@@ -15,9 +15,14 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/about", app.about)
 	router.HandlerFunc(http.MethodGet, "/login", app.loginform)
+	router.HandlerFunc(http.MethodPost, "/login", app.loginform)// function needs to be creates to authenticate user identity
 	router.HandlerFunc(http.MethodGet, "/register", app.register)
+	router.HandlerFunc(http.MethodPost, "/register", app.register) // function needs to be created to save registration data 
 	router.HandlerFunc(http.MethodGet, "/reservation", app.reserve)
-	router.HandlerFunc(http.MethodPost, "/feedback", app.feedback)
+	router.HandlerFunc(http.MethodPost, "/reservation", app.reserve) // function needed to be created to save reservation (create handler)
+	router.HandlerFunc(http.MethodGet, "/feedback", app.feedback)
+	router.HandlerFunc(http.MethodPost, "/feedback", app.feedback)// function need to be created to save the feedback given 
+
 	router.HandlerFunc(http.MethodPost, "/user", app.userPortal)
 	router.HandlerFunc(http.MethodPost, "/admin", app.adminPortal)
 
